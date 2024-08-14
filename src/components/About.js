9dc50c60bcff39e8de87ae1b6eb569d0dab5203f
barkley-wq <igonyibarkley00@gmail.com>
2024-08-14 13:31:20 +0300

    index.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,15 @@ import { image } from "../data/data";
 
 function About() {
   return <div>About</div>;
+  return(
+  <>
+  <div id="about">
+      <h2>About Me</h2>
+      <p>The Wekeend</p>
+      <img src={image} alt= "I made this"/>
+  </div>
+  </>
+  );
 }
 
 export default About;
